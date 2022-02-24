300bfc8097a11e8553a37b88db2d1b148a3f52a7
Geo18G <edgar.guerrero8023@alumnos.udg.mx>
2022-02-24 17:23:39 -0600

    Cambios

--- Ejercicios/Ejercicio-1.php
+++ Ejercicios/Ejercicio-1.php
@@ -3,14 +3,5 @@
 $pais = "Italia";
 $continente = "Europa";
 echo "<h1>$pais - $continente<h1/>";
-echo "<h1>$pais - $continente<h1/>";
-echo "<h1>$pais - $continente<h1/>";
-echo "<h1>$pais - $continente<h1/>";
-echo "<h1>$pais - $continente<h1/>";
-echo "<h1>$pais - $continente<h1/>";
-echo "<h1>$pais - $continente<h1/>";
-echo "<h1>$pais - $continente<h1/>";
-echo "<h1>$pais - $continente<h1/>";
-echo "<h1>$pais - $continente<h1/>";
 
 ?>
